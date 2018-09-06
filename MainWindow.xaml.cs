6f2adb5bdfb3d5c522a2f84313ef0a431903bf27
fialkoa <43009411+fialkoa@users.noreply.github.com>
2018-09-06 12:28:42 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace bookreader1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tv_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            //int id = Convert.ToInt32(tv.SelectedValue);
+            //MessageBox.Show(id.ToString());
+            var key = tv.SelectedValue;
+            string path = @"..\..\pages\" + key.ToString() + ".xaml";
+            fdr.Document = (FlowDocument)XamlReader.Load(File.OpenRead(path));
+        }
+    }
+}
